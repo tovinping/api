da89b43747db1620c23806f0cf67d76b7b85ba5e
macMini <tovinping@qq.com>
2024-10-27 13:41:00 +0800

    test

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,6 +1,6 @@
 import { ILoginRes } from "@/types/auth";
 import apiUtils from "@/utils/api";
 export default apiUtils((req, res) => {
-  const loginRes: ILoginRes = { token: "jsonwebtokenAAA" };
+  const loginRes: ILoginRes = { token: "test" };
   res.ok<ILoginRes>(loginRes);
 });
