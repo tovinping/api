946b920adb8cc8313c3a61216f42011c7313ebd9
macMini <tovinping@qq.com>
2025-07-13 01:01:11 +0800

    类型体操

---
+++ app/(groupNote)/groupNote/page.tsx
@@ -0,0 +1,21 @@
+export default function GroupNote() {
+  return <section className="p-20 h-full">
+    <textarea placeholder="请输入内容" name="title" id="title" className="text-orange-300 bg-white p-4 resize-none w-full outline-0" /> 
+    <ul>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+      <li className="h-40 w-36"></li>
+    </ul>
+  </section>
+}
